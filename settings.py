5b71e3d8c6cb25631005136582a47685409f33ff
Michael Kwaku Aboagye <35949773+Rev0kz@users.noreply.github.com>
2020-07-28 07:41:28 +0000

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'supermarket',  #market is the name of the database
+        'USER': 'sysadmin', #sysadmin is the user 
+        'PASSWORD': 'sysmap',
+        'HOST': 'localhost',
+        'PORT': ''
+    }
+}
+
